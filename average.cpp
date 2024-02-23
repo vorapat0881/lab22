5730b7bb449b63bde0390cca06f5bd0eb4fbc593
vorapat0881 <vorapat_g@cmu.ac.th>
2024-02-23 15:21:20 +0700

    average

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+    double a = 0;
+    for(int i=1; i<argc; i++){
+    a += atoi(argv[i]);
+    }
+    if(argc <= 1)cout << "Please input numbers to find average.\n";
+        else {cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << a/(argc-1) << "\n";
+        cout << "---------------------------------";
+        }
+    return 0;
+}
